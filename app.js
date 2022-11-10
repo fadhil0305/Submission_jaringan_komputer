034a39b83f2b416552c89c1c8eb91d0669907cd1
fadhil0305 <52478779+fadhil0305@users.noreply.github.com>
2022-11-10 21:45:11 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+use strict'
+
+const express = require('express')
+
+const PORT = 8000
+const HOST = '0.0.0.0'
+
+const app = express()
+app.get('/', (req, res) => {
+    res.send('Muhammad Fadhil!\n')
+})
+
+app.listen(PORT, HOST)
